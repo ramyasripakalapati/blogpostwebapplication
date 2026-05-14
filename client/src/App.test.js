8c75eada2f9f6f10071f772453238629bded7462
ramyasripakalapati <145890600+ramyasripakalapati@users.noreply.github.com>
2026-05-14 22:41:18 +0530

    Delete client directory

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders create a post link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/create a post/i); // Update this with a text you expect to find in the app
-  expect(linkElement).toBeInTheDocument();
-});
