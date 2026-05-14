8e8b0d7006ca6cda3cd92c93fd9066eb9be8cde2
ramyasripakalapati <145890600+ramyasripakalapati@users.noreply.github.com>
2026-05-14 22:41:54 +0530

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerController, loginController } = require("../controllers/authController");
-
-router.post("/register", registerController);
-router.post("/login", loginController);
-
-module.exports = router;
